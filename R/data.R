4f619ceda171e0c0763c7a1312a6e6fc97c18ce5
Joshua Kunst <jbkunst@gmail.com>
2023-04-15 18:47:20 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,6 +1,9 @@
+# setup -------------------------------------------------------------------
 library(tidyverse)
+library(sf)
 
-data <- readxl::read_excel("data/cartera_proyectos 2023-02-02 15_53_15.xlsx", skip = 2)
+# data --------------------------------------------------------------------
+data <- readxl::read_excel("data/cartera_proyectos_02_23.xlsx")
 
 glimpse(data)
 
@@ -8,4 +11,38 @@ data <- janitor::clean_names(data)
 
 glimpse(data)
 
-data
+data <- data |>
+  select(codigo, ano, provincia, comuna, sector, sub_sector)
+
+tbl_geoserver <- readr::read_csv("data/tbl_geoserver.csv")
+
+data_geo <- tbl_geoserver |>
+  filter(resultado == "descargado") |>
+  pull(nombre) |>
+  str_remove_all("sit\\:") |>
+  map(function(file = "idis_ptomigra"){
+
+    cli::cli_progress_step(file)
+
+    l <- sf::st_read(fs::path("data", "geojson", file, ext = "geojson"), quiet = TRUE)
+
+    l <- l |>
+      mutate(cod_bip = as.character(cod_bip))
+
+    dout <- inner_join(l, data, by = join_by(cod_bip == codigo)) |>
+      mutate(archivo = file, .before = 1) |>
+      mutate(id = as.character(id))
+
+    # mapview::mapview(dout)
+
+    dout
+
+  }) |>
+  bind_rows()
+
+data_geo |>
+  as_tibble() |>
+  count(archivo)
+
+mapview::mapview(data_geo)
+
